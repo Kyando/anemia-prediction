d803c79f1493a8664d349a732a0b3a1bf3474aee
Bruno Ribeiro <Brunoribeiro47@hotmail.com>
2024-05-21 22:08:04 -0300

    added model b with cross-dataset validation

--- src/anemia/anemia_cross_dataset_validation.py
+++ src/anemia/anemia_cross_dataset_validation.py
@@ -52,4 +52,38 @@ if __name__ == '__main__':
     plt.savefig(f"output/{model_name}_tree.png")
     plt.close()
 
-    print("finished")
+    print("finished model A")
+
+    train_X_B, train_Y_B, test_X_B, test_Y_B, preprocessor_B = data_utils.split_train_test_data(data_frame=df_two,
+                                                                                                test_size=0.3,
+                                                                                                y_class="Anemia")
+    x_one_preprocessed, y_one, _ = data_utils.preprocess_data(df_one, "Anemia", preprocessor_B)
+
+    model = "DecisionTree"
+    model_name = "Dataset_B_DecisionTree_B"
+    y_pred_B, trained_model_B = model_utils.train_model(train_X_B, train_Y_B, test_X_B, test_Y_B, model_name=model)
+
+    output_plot_file = f"output/{model_name}_confusion_matrix.png"
+    output_metrics_file = f"output/{model_name}_metrics.png"
+    model_utils.plot_confusion_matrix(test_Y=test_Y_B, y_pred=y_pred_B, labels=labels, model_name=model,
+                                      output_file=output_plot_file, metrics_file=output_metrics_file)
+
+    plt.figure(figsize=(20, 10))
+    plot_tree(trained_model_B, filled=True, feature_names=df_two.columns, class_names=labels, rounded=True)
+    plt.title("Decision Tree")
+    plt.savefig(f"output/{model}_tree.png")
+    plt.close()
+
+    model_name = "Dataset_A_DecisionTree_B"
+    output_plot_file = f"output/{model_name}_confusion_matrix.png"
+    output_metrics_file = f"output/{model_name}_metrics.png"
+
+    y_pred_one = trained_model.predict(x_one_preprocessed)
+    model_utils.plot_confusion_matrix(test_Y=y_one, y_pred=y_pred_one, labels=labels, model_name=model,
+                                      output_file=output_plot_file, metrics_file=output_metrics_file)
+
+    plt.figure(figsize=(20, 10))
+    plot_tree(trained_model, filled=True, feature_names=df_one.columns, class_names=labels, rounded=True)
+    plt.title("Decision Tree")
+    plt.savefig(f"output/{model_name}_tree.png")
+    plt.close()
